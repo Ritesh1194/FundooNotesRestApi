cf8858f5e191a0511561cb586c4205fe4df12096
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-16 09:53:31 +0530

    [Ritesh Yadav]. Updated Response Class In Responses Package

---
+++ src/main/java/com/bridgelabz/responses/Response.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.responses;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Response {
+
+	private String message;
+
+	private int statusCode;
+
+	private Object obj;
+
+	List<String> details;
+
+	public Response(String message, int statusCode) {
+
+		this.message = message;
+
+		this.statusCode = statusCode;
+	}
+
+	public Response(String message, int statusCode, Object obj) {
+		this.message = message;
+		this.statusCode = statusCode;
+		this.obj = obj;
+	}
+}
