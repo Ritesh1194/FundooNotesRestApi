c60b69ffced5082979d6969431762dac387c470a
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-16 09:33:55 +0530

    [Ritesh Yadav]. Updated UserException Class In  Exception Package

---
+++ src/main/java/com/bridgelabz/exception/UserException.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.exception;
+
+import lombok.Getter;
+
+@Getter
+public class UserException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	private String message;
+
+	public UserException(String message) {
+		super(message);
+		this.message = message;
+	}
+}
