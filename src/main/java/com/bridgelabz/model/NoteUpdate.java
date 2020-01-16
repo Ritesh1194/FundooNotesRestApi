00664e14300579c158e8a4896579e131c4c0da17
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-16 09:40:18 +0530

    [Ritesh Yadav]. Updated NoteUpdate Class In Model Package

---
+++ src/main/java/com/bridgelabz/model/NoteUpdate.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.model;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class NoteUpdate {
+	private long id;
+
+	private String title;
+
+	private String description;
+
+	private boolean isArchieved;
+
+	private boolean isPinned;
+
+	private boolean isTrashed;
+
+	private LocalDateTime createdDateAndTime;
+
+	private LocalDateTime upDateAndTime;
+}
