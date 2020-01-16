565dfa0f7d200e76ccda85acd170ccfa4979c13e
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-16 09:46:19 +0530

    [Ritesh Yadav]. Updated IUserRepository In Repository Package

---
+++ src/main/java/com/bridgelabz/repository/IUserRepository.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.repository;
+
+import java.util.List;
+import com.bridgelabz.model.PasswordUpdate;
+import com.bridgelabz.model.User;
+
+public interface IUserRepository {
+
+	public User save(User userInformation);
+
+	public User getUser(String email);
+
+	boolean verify(Long id);
+
+	boolean upDate(PasswordUpdate information, Long token);
+
+	User getUserById(Long id);
+
+	List<User> getUsers();
+}
