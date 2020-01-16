8e536d23798313e0cd1bd0bc3d11c2e59506dcf4
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-16 19:29:26 +0530

    [Ritesh Yadav].add FundooNotesApiApplicationTests Class In FundooNotesApi

---
+++ src/test/java/com/bridgelabz/FundooNotesApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FundooNotesApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+}
