452ce938e6c41e7f4f36a9add7130b691fe133ea
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-16 09:54:21 +0530

    [Ritesh Yadav]. Updated UserDetailClass In Responses Package

---
+++ src/main/java/com/bridgelabz/responses/UsersDetail.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.responses;
+
+import lombok.Data;
+
+@Data
+public class UsersDetail {
+
+	private String token;
+
+	private int statuscode;
+
+	private Object obj;
+
+	public UsersDetail(String token, int statuscode, Object obj) {
+		this.token = token;
+
+		this.statuscode = statuscode;
+
+		this.obj = obj;
+	}
+}
