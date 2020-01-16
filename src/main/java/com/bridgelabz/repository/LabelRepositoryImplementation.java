e47f7f087dc71a70a95b729ad832823cd08571d5
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-16 17:54:51 +0530

    [Ritesh Yadav]. Updated labelRepositoryImplementation Class in Repository Package

--- src/main/java/com/bridgelabz/repository/LabelRepositoryImplementation.java
+++ src/main/java/com/bridgelabz/repository/LabelRepositoryImplementation.java
@@ -16,7 +16,7 @@ public class LabelRepositoryImplementation implements ILabelRepository {
 	EntityManager entityManager;
 
 	@Override
-	public Label save(Label labelInformation) {
+	public Label saveLabel(Label labelInformation) {
 		Session session = entityManager.unwrap(Session.class);
 		session.save(labelInformation);
 		return labelInformation;
@@ -30,23 +30,23 @@ public class LabelRepositoryImplementation implements ILabelRepository {
 	}
 
 	@Override
-	public Label fetchLabel(Long userid, String labelname) {
+	public Label fetchLabel(Long userId, String name) {
 		Session session = entityManager.unwrap(Session.class);
-		Query q = session.createQuery("from Label where user_id=:id and name=:name");
-		q.setParameter("id", userid);
-		q.setParameter("name", labelname);
-		return (Label) q.getResultList();
+		Query q = session.createQuery("from Label where user_id=:user_id and name=:name");
+		q.setParameter("user_id", userId);
+		q.setParameter("name", name);
+		return (Label) q.uniqueResult();
 	}
 
 	@Override
 	public Label getLabel(Long id) {
-		String hql = "FROM Label " + "WHERE label_id=:id";
+		String hql = "FROM Label " + "WHERE label_id=:label_id";
 		Session session = entityManager.unwrap(Session.class);
-		return (Label) session.createQuery("from Label where label_id='" + id + "'").uniqueResult();
+		return (Label) session.createQuery("from Label where label_id='" + id + "'").getResultList();
 	}
 
 	@Override
-	public Label update(Integer labelId, Label label, String token) {
+	public Label updateLabel(Integer labelId, Label label, String token) {
 		Session currentSession = entityManager.unwrap(Session.class);
 		Label labelObj = currentSession.get(Label.class, labelId);
 		if (labelObj != null) {
@@ -59,25 +59,25 @@ public class LabelRepositoryImplementation implements ILabelRepository {
 	@Override
 	public Label fetchLabelById(long id) {
 		Session session = entityManager.unwrap(Session.class);
-		Query q = session.createQuery("from Label where label_id=:id");
-		q.setParameter("id", id);
+		Query q = session.createQuery("from Label where label_id=:label_id");
+		q.setParameter("label_id", id);
 		return (Label) q.getResultList();
 	}
 
 	@Override
 	public int deleteLabel(long i) {
-		String hql = "DELETE FROM Label " + "WHERE label_id =:id";
+		String hql = "DELETE FROM Label " + "WHERE label_id =:label_id";
 		Session session = entityManager.unwrap(Session.class);
 		Query query = session.createQuery(hql);
-		query.setParameter("id", i);
+		query.setParameter("label_id", i);
 		int result = query.executeUpdate();
 		return result;
 	}
 
 	@Override
 	public List<Label> getAllLabel(long id) {
-		String hql = "FROM Label " + "WHERE user_id=:id";
+		String hql = "FROM Label " + "WHERE user_id=:user_id";
 		Session session = entityManager.unwrap(Session.class);
-		return session.createQuery("from Label where user_Id='" + id + "'").getResultList();
+		return session.createQuery("from Label where user_id='" + id + "'").getResultList();
 	}
 }
