575b40bc4c5fa28a9ef61d96016af57f83f64454
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-18 15:39:40 +0530

    [Ritesh Yadav]add. Updated UserRepositoryImplemantation  In Repository Package

--- src/main/java/com/bridgelabz/repository/UserRepositoryImplementation.java
+++ src/main/java/com/bridgelabz/repository/UserRepositoryImplementation.java
@@ -8,6 +8,7 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bridgelabz.model.PasswordUpdate;
 import com.bridgelabz.model.User;
@@ -18,16 +19,20 @@ public class UserRepositoryImplementation implements IUserRepository {
 	@Autowired
 	private EntityManager entityManager;
 
+	@Transactional
 	@Override
 	public User save(User userInformation) {
+
 		Session session = entityManager.unwrap(Session.class);
 		session.saveOrUpdate(userInformation);
 		return userInformation;
 
 	}
 
+	@Transactional
 	@Override
 	public User getUser(String email) {
+
 		Session session = entityManager.unwrap(Session.class);
 		Query q = session.createQuery(" FROM User where email=:email");
 		q.setParameter("email", email);
@@ -35,23 +40,24 @@ public class UserRepositoryImplementation implements IUserRepository {
 
 	}
 
+	@Transactional
 	@Override
 	public User getUserById(Long id) {
-
 		Session session = entityManager.unwrap(Session.class);
-		Query q = session.createQuery(" FROM User where id=:id");
-		q.setParameter("id", id);
+		Query q = session.createQuery(" FROM User where userId=:userId");
+		q.setParameter("userId", id);
 		return (User) q.uniqueResult();
 
 	}
 
+	@Transactional
 	@Override
 	public boolean upDate(PasswordUpdate information, Long id) {
 
 		Session session = entityManager.unwrap(Session.class);
-		Query q = session.createQuery("update User set password=:p" + " " + " " + "where id=:i");
-		q.setParameter("p", information.getConfirmPassword());
-		q.setParameter("i", id);
+		Query q = session.createQuery("update User set password=:password" + " " + " " + "where userId=:userId");
+		q.setParameter("password", information.getConfirmPassword());
+		q.setParameter("userId", id);
 
 		int status = q.executeUpdate();
 		if (status > 0) {
@@ -61,13 +67,14 @@ public class UserRepositoryImplementation implements IUserRepository {
 		}
 	}
 
+	@Transactional
 	@Override
 	public boolean verify(Long id) {
 
 		Session session = entityManager.unwrap(Session.class);
-		Query q = session.createQuery("update User set is_verified=:p" + " " + " " + "where id=:i");
-		q.setParameter("p", true);
-		q.setParameter("i", id);
+		Query q = session.createQuery("update User set is_verified=:is_verified" + " " + " " + "where userId=:userId");
+		q.setParameter("is_verified", true);
+		q.setParameter("userId", id);
 
 		int status = q.executeUpdate();
 		if (status > 0) {
@@ -75,8 +82,10 @@ public class UserRepositoryImplementation implements IUserRepository {
 		} else {
 			return false;
 		}
+
 	}
 
+	@Transactional
 	@Override
 	public List<User> getUsers() {
 		Session currentsession = entityManager.unwrap(Session.class);
