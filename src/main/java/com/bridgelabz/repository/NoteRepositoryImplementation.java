4b3b57436b7fd3f37cf0910efea83b13b00dbc4d
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-18 15:40:45 +0530

    [Ritesh Yadav]add. Updated NoteRepositoryImplemantation Class  In Repository Package

--- src/main/java/com/bridgelabz/repository/NoteRepositoryImplementation.java
+++ src/main/java/com/bridgelabz/repository/NoteRepositoryImplementation.java
@@ -1,9 +1,8 @@
 package com.bridgelabz.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
-
 import javax.persistence.EntityManager;
-
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +18,7 @@ public class NoteRepositoryImplementation implements INoteRepository {
 
 	@Transactional
 	@Override
-	public Note saveNote(Note note) {
+	public Note createNote(Note note) {
 		Session session = entityManager.unwrap(Session.class);
 		session.saveOrUpdate(note);
 		return note;
@@ -27,20 +26,21 @@ public class NoteRepositoryImplementation implements INoteRepository {
 
 	@Transactional
 	@Override
-	public Note findById(long id) {
+	public Note findById(long noteId) {
 		Session session = entityManager.unwrap(Session.class);
-		Query query = (Query) session.createQuery("from Note where note_id=:note_id");
-		query.setParameter("note_id", id);
+		Query query = (Query) session.createQuery("from Note where noteId=:noteId");
+		query.setParameter("noteId", noteId);
 		return (Note) query.uniqueResult();
 	}
 
 	@Transactional
 	@Override
-	public boolean deleteNote(long id, long userid) {
+	public boolean deleteNote(long id, Note note) {
+		System.out.println("Delete Node3");
 		Session session = entityManager.unwrap(Session.class);
-		String hql = "DELETE FROM Note " + "WHERE note_id = :note_id";
+		String hql = "DELETE FROM Note " + "WHERE noteId = :noteId";
 		Query query = session.createQuery(hql);
-		query.setParameter("note_id", id);
+		query.setParameter("noteId", id);
 		int result = query.executeUpdate();
 		if (result >= 1) {
 			return true;
@@ -52,11 +52,10 @@ public class NoteRepositoryImplementation implements INoteRepository {
 	@Transactional
 	@Override
 	public List<Note> getNotes(long userid) {
-
 		System.out.println("in repository");
 		Session session = entityManager.unwrap(Session.class);
-		return session.createQuery("from Note where note_id='" + userid + "'"
-				+ " and is_trashed=false and is_archieved=false ORDER BY id DESC").list();
+		return session.createQuery("from Note where noteId='" + userid + "'"
+				+ " and is_trashed=false and is_archieved=false ORDER BY id DESC").getResultList();
 	}
 
 	@Transactional
@@ -64,8 +63,7 @@ public class NoteRepositoryImplementation implements INoteRepository {
 	public List<Note> getTrashedNotes(long userid) {
 		System.out.println("in repository");
 		Session session = entityManager.unwrap(Session.class);
-
-		return session.createQuery("from Note where id='" + userid + "'" + " and is_trashed=true").list();
+		return session.createQuery("from Note where user_id='" + userid + "'" + " and is_trashed=true").getResultList();
 	}
 
 	@Transactional
@@ -76,10 +74,12 @@ public class NoteRepositoryImplementation implements INoteRepository {
 		Session session = entityManager.unwrap(Session.class);
 
 		return session
-				.createQuery("from Note where id='" + userid + "'" + " and is_archieved=true" + " and is_trashed=false")
-				.list();
+				.createQuery(
+						"from Note where userId='" + userid + "'" + " and is_archieved=true" + " and is_trashed=false")
+				.getResultList();
 	}
 
+	@Transactional
 	@Override
 	public boolean updateColor(long id, long userid, String colour) {
 
@@ -96,4 +96,62 @@ public class NoteRepositoryImplementation implements INoteRepository {
 			return false;
 		}
 	}
+
+	@Transactional
+	@Override
+	public List<Note> getNotesOfSameLabel(Long userId, Long labelId) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		Query<Note> query = currentSession.createQuery("from Note where user_id" + userId + "and labelId=" + labelId,
+				Note.class);
+		return query.getResultList();
+	}
+
+//	@Transactional
+//	@Override
+//	public boolean setRestored(Long userId, Long noteId) {
+//		System.out.println();
+//		Session currentSession = entityManager.unwrap(Session.class);
+//		Note note = findById(noteId);
+//		if (note.getUserId().equals(userId)) {
+//			if (note.isTrashed()) {
+//				note.setTrashed(false);
+//				note.setCreatedDateAndTime(LocalDateTime.now());
+//				currentSession.saveOrUpdate(note);
+//				return true;
+//			}
+//			return false;
+//		}
+//
+//		return false;
+//	}
+//
+//	@Override
+//	public boolean setTrashed(Long userId, Long noteId) {
+//		Session currentSession = entityManager.unwrap(Session.class);
+//		Note note = findById(noteId);
+//		if (note.getUserId().equals(userId)) {
+//			if (!note.isTrashed()) {
+//				note.setTrashed(true);
+//				note.setCreatedDateAndTime(LocalDateTime.now());
+//				currentSession.save(note);
+//				return true;
+//			}
+//			return false;
+//		}
+//		return false;
+//	}
+
+	@Override
+	public boolean setRemaineder(Long userId, Long noteId, LocalDateTime time) {
+		Session currentSession = entityManager.unwrap(Session.class);
+		Note note = findById(noteId);
+		if (note.getUserId().equals(userId)) {
+			note.setReminder(time);
+			;
+			currentSession.save(note);
+			return true;
+		}
+		return false;
+	}
+
 }
