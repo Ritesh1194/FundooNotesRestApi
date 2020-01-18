f4d61895412ae9150d7c38fbca9e27f32bc4b30a
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-18 15:37:38 +0530

    [Ritesh Yadav]add. Updated INoteRepository Interface In Repository Package

--- src/main/java/com/bridgelabz/repository/INoteRepository.java
+++ src/main/java/com/bridgelabz/repository/INoteRepository.java
@@ -1,15 +1,16 @@
 package com.bridgelabz.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.bridgelabz.model.Note;
 
 public interface INoteRepository {
-	public Note saveNote(Note note);
+	public Note createNote(Note note);
 
-	public Note findById(long id);
+	public Note findById(long noteId);
 
-	public boolean deleteNote(long id, long userId);
+	public boolean deleteNote(long id, Note note);
 
 	public List<Note> getNotes(long userId);
 
@@ -18,4 +19,12 @@ public interface INoteRepository {
 	public List<Note> getArchiveNotes(long userId);
 
 	public boolean updateColor(long userId, long id, String color);
+
+	public List<Note> getNotesOfSameLabel(Long userId, Long labelId);
+
+//	public boolean setRestored(Long userId, Long noteId);
+//
+//	public boolean setTrashed(Long userId, Long noteId);
+
+	public boolean setRemaineder(Long userId, Long noteId, LocalDateTime time);
 }
