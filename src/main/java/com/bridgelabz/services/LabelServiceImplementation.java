c29017d5e928135056531e7aa8014eb738956c18
Riteh1194 <yadavritesh1194@gmail.com>
2020-01-18 15:47:44 +0530

    [Ritesh Yadav]add. Updated LabelServicesImplemantation In Services Package

--- src/main/java/com/bridgelabz/services/LabelServiceImplementation.java
+++ src/main/java/com/bridgelabz/services/LabelServiceImplementation.java
@@ -1,24 +1,28 @@
 package com.bridgelabz.services;
 
 import java.util.List;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.bridgelabz.exception.UserException;
 import com.bridgelabz.model.Label;
 import com.bridgelabz.model.LabelDto;
 import com.bridgelabz.model.LabelUpdate;
 import com.bridgelabz.model.Note;
 import com.bridgelabz.model.User;
-import com.bridgelabz.repository.LabelRepositoryImplementation;
-import com.bridgelabz.repository.NoteRepositoryImplementation;
+import com.bridgelabz.repository.ILabelRepository;
+import com.bridgelabz.repository.INoteRepository;
 import com.bridgelabz.repository.UserRepositoryImplementation;
 import com.bridgelabz.util.JwtGenerator;
 
 @Service
 public class LabelServiceImplementation implements ILabelService {
 	@Autowired
-	private LabelRepositoryImplementation labelRepositoryImplementation;
+	private ILabelRepository labelRepository;
 
 	private Label labelInformation = new Label();
 	@Autowired
@@ -26,8 +30,12 @@ public class LabelServiceImplementation implements ILabelService {
 	@Autowired
 	private JwtGenerator tokenGenerator;
 	@Autowired
-	private NoteRepositoryImplementation noteRepositoryImplementation;
+	private INoteRepository noteRepositoryImplementation;
 
+	@Autowired
+	ModelMapper modelMapper;
+
+	@Transactional
 	@Override
 	public boolean createLabel(LabelDto label, String token) {
 		Long id = null;
@@ -44,17 +52,18 @@ public class LabelServiceImplementation implements ILabelService {
 		System.out.println("Inside Label2 ");
 		if (user != null) {
 			System.out.println("Inside Label3 ");
-			Label labelInfo = labelRepositoryImplementation.fetchLabel(user.getUserId(), label.getName());
+			Label labelInfo = labelRepository.fetchLabel(user.getUserId(), label.getName());
 			System.out.println("Inside Label ");
 			if (labelInfo == null) {
 				Label labell = new Label();
 
-				// labelInformation = modelMapper.map(label, Label.class);
-				BeanUtils.copyProperties(label, labell);
-				labelInformation.getLabelId();
-				labelInformation.getName();
+				labelInformation = modelMapper.map(label, Label.class);
+				// BeanUtils.copyProperties(label, labell);
+//				labelInformation.getLabelId();
+//				labelInformation.getName();
 				labelInformation.setUserId(user.getUserId());
-				labelRepositoryImplementation.saveLabel(labelInformation);
+				// labelRepository.saveLabel(labell);
+				labelRepository.saveLabel(labelInformation);
 			} else {
 				throw new UserException("label with the given name is already present");
 			}
@@ -64,6 +73,7 @@ public class LabelServiceImplementation implements ILabelService {
 		return true;
 	}
 
+	@Transactional
 	@Override
 	public boolean createLabelAndMap(LabelDto label, String token, Long noteId) {
 		Long id = null;
@@ -77,14 +87,14 @@ public class LabelServiceImplementation implements ILabelService {
 
 		User user = userRepository.getUserById(id);
 		if (user != null) {
-			Label labelInfo = labelRepositoryImplementation.fetchLabel(user.getUserId(), label.getName());
+			Label labelInfo = labelRepository.fetchLabel(user.getUserId(), label.getName());
 			if (labelInfo == null) {
 				BeanUtils.copyProperties(label, Label.class);
 				labelInformation.setUserId(user.getUserId());
-				labelRepositoryImplementation.saveLabel(labelInformation);
+				labelRepository.saveLabel(labelInformation);
 				Note note = noteRepositoryImplementation.findById(noteId);
 				note.getList().add(labelInformation);
-				noteRepositoryImplementation.saveNote(note);
+				noteRepositoryImplementation.createNote(note);
 
 			} else {
 				throw new UserException("label with the given name is already present");
@@ -95,24 +105,30 @@ public class LabelServiceImplementation implements ILabelService {
 		return true;
 	}
 
+	@Transactional
 	@Override
 	public boolean addLabel(Long labelId, Long noteId, String token) {
 		Note note = noteRepositoryImplementation.findById(noteId);
-		Label label = labelRepositoryImplementation.fetchLabelById(labelId);
+		System.out.println("add Label1");
+		Label label = labelRepository.fetchLabelById(labelId);
 		label.getList().add(note);
-		labelRepositoryImplementation.saveLabel(label);
+		System.out.println("add Label 2");
+		labelRepository.saveLabel(label);
+		System.out.println();
 		return true;
 	}
 
+	@Transactional
 	@Override
 	public boolean removeLabel(Long labelId, Long noteId, String token) {
 		Note note = noteRepositoryImplementation.findById(noteId);
-		Label label = labelRepositoryImplementation.fetchLabelById(labelId);
+		Label label = labelRepository.fetchLabelById(labelId);
 		note.getList().remove(label);
-		labelRepositoryImplementation.saveLabel(label);
+		labelRepository.saveLabel(label);
 		return true;
 	}
 
+	@Transactional
 	@Override
 	public boolean editLabel(LabelUpdate label, String token) {
 		Long id = null;
@@ -126,10 +142,10 @@ public class LabelServiceImplementation implements ILabelService {
 
 		User user = userRepository.getUserById(id);
 		if (user != null) {
-			Label labelInfo = labelRepositoryImplementation.fetchLabelById(label.getLableId());
+			Label labelInfo = labelRepository.fetchLabelById(label.getLableId());
 			if (labelInfo != null) {
 				labelInfo.setName(label.getLabelName());
-				labelRepositoryImplementation.saveLabel(labelInfo);
+				labelRepository.saveLabel(labelInfo);
 			} else {
 				throw new UserException("label with the given id does not exist");
 			}
@@ -140,29 +156,29 @@ public class LabelServiceImplementation implements ILabelService {
 		return true;
 	}
 
-	@Override
-	public boolean deleteLabel(LabelUpdate info, String token) {
-		Long id = null;
-
-		try {
-			id = (long) tokenGenerator.parseJWT(token);
-		} catch (Exception e) {
-
-			throw new UserException("User does not exist");
-		}
-
-		User user = userRepository.getUserById(id);
-		if (user != null) {
-			Label labelInfo = labelRepositoryImplementation.fetchLabelById(info.getLableId());
-			if (labelInfo != null) {
-				labelRepositoryImplementation.deleteLabel(info.getLableId());
-			} else {
-				throw new UserException("Note does not exist");
-			}
-		}
-		return true;
-	}
-
+//	@Override
+//	public boolean deleteLabel(LabelUpdate info, String token) {
+//		Long id = null;
+//
+//		try {
+//			id = (long) tokenGenerator.parseJWT(token);
+//		} catch (Exception e) {
+//
+//			throw new UserException("User does not exist");
+//		}
+//
+//		User user = userRepository.getUserById(id);
+//		if (user != null) {
+//			Label labelInfo = labelRepository.fetchLabelById(info.getLableId());
+//			if (labelInfo != null) {
+//				labelRepository.deleteLabel(info.getLableId());
+//			} else {
+//				throw new UserException("Note does not exist");
+//			}
+//		}
+//		return true;
+//	}
+	@Transactional
 	@Override
 	public List<Label> getLabel(String token) {
 
@@ -174,17 +190,17 @@ public class LabelServiceImplementation implements ILabelService {
 			throw new UserException("note does not exist");
 		}
 
-		List<Label> labels = labelRepositoryImplementation.getAllLabel(id);
+		List<Label> labels = labelRepository.getAllLabel(id);
 		return labels;
 
 	}
 
+	@Transactional
 	@Override
 	public List<Note> getAllNotes(String token, Long labelId) {
-		Label label = labelRepositoryImplementation.getLabel(labelId);
+		Label label = labelRepository.getLabel(labelId);
 		List<Note> list = label.getList();
 		System.out.println("label is" + list);
-
 		return list;
 	}
 }
